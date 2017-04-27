1fdeba47772e3b47f23ccd98c0e33761b778db9d
zemkoz <zemko_z@hotmail.com>
2017-04-27 22:08:55 +0200

    Add javadoc to Addition class.

--- src/main/java/cz/zemko/math/Addition.java
+++ src/main/java/cz/zemko/math/Addition.java
@@ -1,7 +1,23 @@
 package cz.zemko.math;
 
+/**
+ * Provides additional operation.
+ *
+ * @author Zoltan Zemko
+ */
 public final class Addition {
 
+    private Addition() {
+        // Can't be instantiated.
+    }
+
+    /**
+     * Compute sum of two values.
+     *
+     * @param a value
+     * @param b value
+     * @return sum of a and b value.
+     */
     public static int compute(int a, int b) {
         return a + b;
     }
