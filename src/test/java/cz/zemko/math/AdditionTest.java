dc458bd4c99e0d4273063533be1d10f20819cb85
zemkoz <zemko_z@hotmail.com>
2017-04-27 22:00:15 +0200

    Added more unit tests for testing Addition.

--- src/test/java/cz/zemko/math/AdditionTest.java
+++ src/test/java/cz/zemko/math/AdditionTest.java
@@ -4,11 +4,40 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Unit tests for testing {@link Addition}.
+ *
+ * @author Zoltan Zemko
+ */
 public class AdditionTest {
 
     @Test
     public void testCompute_zeroPlusZero() {
         long result = Addition.compute(0, 0);
         assertEquals(0, result);
+    }
+
+    @Test
+    public void testCompute_positiveNumberPlusPositiveNumber() {
+        long result = Addition.compute(5, 7);
+        assertEquals(12, result);
+    }
+
+    @Test
+    public void testCompute_negativeNumberPlusPositiveNumber() {
+        long result = Addition.compute(-5, 7);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testCompute_positiveNumberPlusNegativeNumber() {
+        long result = Addition.compute(3, -8);
+        assertEquals(-5, result);
+    }
+
+    @Test
+    public void testCompute_negativeNumberPlusNegativeNumber() {
+        long result = Addition.compute(-4, -3);
+        assertEquals(-7, result);
     }
 }
