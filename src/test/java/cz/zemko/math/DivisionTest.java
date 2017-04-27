35b21f40a2456590a50ef0ec3a4870f7b529bbcc
zemkoz <zemko_z@hotmail.com>
2017-04-27 22:27:07 +0200

    Created new unit tests for testing Division class.

---
+++ src/test/java/cz/zemko/math/DivisionTest.java
@@ -0,0 +1,32 @@
+package cz.zemko.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DivisionTest {
+
+    @Test
+    public void testCompute_positiveNumberMinusPositiveNumber() {
+        int result = Division.compute(4, 2);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testCompute_negativeNumberMinusPositiveNumber() {
+        int result = Division.compute(-10, 2);
+        assertEquals(-5, result);
+    }
+
+    @Test
+    public void testCompute_positiveNumberMinusNegativeNumber() {
+        int result = Division.compute(9, -3);
+        assertEquals(-3, result);
+    }
+
+    @Test
+    public void testCompute_negativeNumberMinusNegativeNumber() {
+        long result = Division.compute(-16, -4);
+        assertEquals(4, result);
+    }
+}
